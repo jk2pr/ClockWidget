0df4dafe0a851075f57e84d568f66424a6206de9
jk2pr <jk12051985>
2018-05-08 15:13:14 +0800

    Initial commit

---
+++ app/src/main/java/com/jk/widget/AppWidget.kt
@@ -0,0 +1,51 @@
+package com.jk.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [AppWidgetConfigureActivity]
+ */
+class AppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            AppWidgetConfigureActivity.deleteTitlePref(context, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
+                                     appWidgetId: Int) {
+
+            val widgetText = AppWidgetConfigureActivity.loadTitlePref(context, appWidgetId)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.app_widget)
+            //views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
