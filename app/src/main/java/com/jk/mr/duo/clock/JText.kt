dbbb3930909e7d28948019590471f8d46cd83f92
jk2pr <jk2praj@gmail.com>
2018-06-21 11:14:15 +0800

    Added preference activity for more customization

--- app/src/main/java/com/jk/mr/duo/clock/JText.kt
+++ app/src/main/java/com/jk/mr/duo/clock/JText.kt
@@ -7,6 +7,7 @@ import android.widget.TextClock
 class JText : TextClock {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
 
