10777ad0747e51b3ef1d0ebe8e9a70f98bb1262a
Jitendra Prajapati <jk2praj@gmail.com>
2021-11-28 18:46:12 +0800

    Fixed issue when pref is null

--- app/src/main/java/com/jk/mr/duo/clock/utils/AppWidgetHelper.kt
+++ app/src/main/java/com/jk/mr/duo/clock/utils/AppWidgetHelper.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 import com.google.gson.Gson
 import com.jk.mr.duo.clock.AppWidgetConfigureActivity
@@ -24,26 +25,31 @@ class AppWidgetHelper constructor(
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
-
-        val calDataJSON = preferenceHandler.getTimeZonePref(context)
-        val calData = Gson().fromJson(calDataJSON, CalData::class.java)
-        val selectedTimeZone = calData.currentCityTimeZoneId ?: TimeZone.getDefault().id
-
         val views = RemoteViews(context.packageName, R.layout.app_widget)
         themeHandler.setWidgetTheme(context, views, preferenceHandler.getThemePref())
-        setUpViews(views, calData, selectedTimeZone)
-        updateWidget(context, appWidgetId, views, appWidgetManager)
+
+        val calDataJSON = preferenceHandler.getTimeZonePref()
+        calDataJSON?.let {
+            val calData = Gson().fromJson(it, CalData::class.java)
+            val selectedTimeZone = calData.currentCityTimeZoneId ?: TimeZone.getDefault().id
+            setUpViews(views, calData, selectedTimeZone)
+            updateWidget(context, appWidgetId, views, appWidgetManager)
+        }
     }
 }
 
-private fun setUpViews(views: RemoteViews, calData: CalData, selectedTimeZone: String) {
+private fun setUpViews(views: RemoteViews, calData: CalData?, selectedTimeZone: String) {
     views.apply {
         setCharSequence(R.id.clock0, SET_FORMAT12HOUR, Utils.get12HoursFormat())
         setCharSequence(R.id.clock1, SET_FORMAT12HOUR, Utils.get12HoursFormat())
         setCharSequence(R.id.clock0, SET_FORMAT24HOUR, Utils.get24HoursFormat())
         setCharSequence(R.id.clock1, SET_FORMAT24HOUR, Utils.get24HoursFormat())
-        setCharSequence(R.id.txt_timezone0, SET_TEXT, calData.displayTimeZoneCityById())
-        setCharSequence(R.id.txt_timezone1, SET_TEXT, calData.displayTimeZoneCityById(selectedTimeZone))
+        setCharSequence(R.id.txt_timezone0, SET_TEXT, calData?.displayTimeZoneCityById())
+        setCharSequence(
+            R.id.txt_timezone1,
+            SET_TEXT,
+            calData?.displayTimeZoneCityById(selectedTimeZone)
+        )
         setString(R.id.clock0, SET_TIME_ZONE, TimeZone.getDefault().id)
         setString(R.id.clock1, SET_TIME_ZONE, TimeZone.getTimeZone(selectedTimeZone).id)
     }
@@ -57,7 +63,14 @@ private fun updateWidget(
 ) {
     val intent = Intent(context, AppWidgetConfigureActivity::class.java)
     intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    val pendingIntent = PendingIntent.getActivity(
+        context, 0, intent,
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+    )
     views.setOnClickPendingIntent(R.id.rel_custom_time, pendingIntent)
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
