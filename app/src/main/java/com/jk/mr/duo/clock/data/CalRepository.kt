cafecd29b7dd1ce4bdb41fd4cbefdb01c15c9bb5
Jitendra Prajapati <jk2praj@gmail.com>
2022-01-03 19:25:45 +0800

    Minor refactor

--- app/src/main/java/com/jk/mr/duo/clock/data/CalRepository.kt
+++ app/src/main/java/com/jk/mr/duo/clock/data/CalRepository.kt
@@ -4,6 +4,6 @@ import com.jk.mr.duo.clock.BuildConfig
 import com.jk.mr.duo.clock.network.IApi
 
 class CalRepository(private val api: IApi) {
-    suspend fun getTimeZone(location: String) = api.getTimeZoneFromLatLong(location, BuildConfig.MAP_TIMEZONE_KEY)
+    suspend fun getTimeZone(location: String) = api.getTimeZoneFromLatLong(location, BuildConfig.BING_MAP_KEY)
     suspend fun getFlag(countryString: MutableMap<String, String>) = api.getFlag(query = countryString)
 }
